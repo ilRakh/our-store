96f0067729a5356c533ebb35507a87f6e75c0cce
Camilo Sanchez <camilo_sanchezcjs29@hotmail.com>
2021-07-15 22:47:42 -0300

    Agregue plantilla de la seccion de tienda

---
+++ mainStore/js/materialize-tools.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible.expandable');
+    var instances = M.Collapsible.init(elems, options);
+});
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
+
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+});
+
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+});
